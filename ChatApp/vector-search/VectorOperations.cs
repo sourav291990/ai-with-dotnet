9c2cc06621dbd3247750f36b212f307d8b26daa3
Sourav Singha <sourav291990@gmail.com>
2025-06-29 21:32:39 +0530

    Changes for runnign use vector using ollama

--- ChatApp/vector-search/VectorOperations.cs
+++ ChatApp/vector-search/VectorOperations.cs
@@ -2,9 +2,8 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.VectorData;
 using Microsoft.SemanticKernel.Connectors.InMemory;
-using OpenAI;
-using System.ClientModel;
-using System.Reflection.Emit;
+using OllamaSharp;
+// using OpenAI;
 
 namespace ChatApp.vector_search
 {
@@ -13,18 +12,23 @@ namespace ChatApp.vector_search
 
 
         private readonly IConfigurationRoot config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
-        private readonly string _openAIKey;
+        // private readonly string _openAIKey;
         private readonly string _embeddingModelName;
+        private readonly string _ollamaUri;
         private VectorStoreCollection<int, CloudService> _cloudServiceStore;
-        private readonly IEmbeddingGenerator<string, Embedding<float>> _generator;
+        //private readonly IEmbeddingGenerator<string, Embedding<float>> _generator;
+        private readonly OllamaApiClient _ollamaClient;
 
         public VectorOperations()
         {
             _embeddingModelName = config["EmbeddingModelName"];
-            _openAIKey = config["OpenAIKey"];
-            _generator = new OpenAIClient(new ApiKeyCredential(_openAIKey))
-                .GetEmbeddingClient(_embeddingModelName)
-                .AsIEmbeddingGenerator();
+            // Below two lines are for OpenAI embedding generation
+            //_openAIKey = config["OpenAIKey"];
+            //_generator = new OpenAIClient(new ApiKeyCredential(_openAIKey))
+            //    .GetEmbeddingClient(_embeddingModelName)
+            //    .AsIEmbeddingGenerator();
+            _ollamaUri = config["OllamaURI"];
+            _ollamaClient = new OllamaApiClient(new Uri(_ollamaUri), _embeddingModelName);
 
         }
 
@@ -36,14 +40,14 @@ namespace ChatApp.vector_search
 
             foreach (CloudService service in cloudServices)
             {
-                service.Vector = await _generator.GenerateVectorAsync(service.Description);
+                service.Vector = await _ollamaClient.GenerateVectorAsync(service.Description);
                 await _cloudServiceStore.UpsertAsync(service);
             }
         }
 
         public async Task<ReadOnlyMemory<float>> GenerateEmbeddingsFromQuery(string query)
         {
-            return await _generator.GenerateVectorAsync(query);
+            return await _ollamaClient.GenerateVectorAsync(query);
         }
 
         public IAsyncEnumerable<VectorSearchResult<CloudService>> Search(ReadOnlyMemory<float> queryEmbedding)
